42d6ccd33e982c34efe4b0ba0a1664470be6be8b
John Malc <cincenko@outlook.com>
2018-08-30 18:28:44 +0200

    fix

--- b40_cz/settings.py
+++ b40_cz/settings.py
@@ -13,7 +13,6 @@ import os
 import sys
 import dj_database_url
 import django_heroku
-import django-bootstrap4
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
