411abcb3f22d74c63e670bcaa6ef3d08046b7dd5
John Malc <cincenko@outlook.com>
2018-08-31 17:55:29 +0200

    temp

--- b40_cz/settings.py
+++ b40_cz/settings.py
@@ -127,8 +127,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
