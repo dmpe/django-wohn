58d60498c01523d83edf142ca6cc03763ecd2eff
John Malc <cincenko@outlook.com>
2018-09-03 18:21:46 +0200

    add proper import url

--- b40_cz/urls.py
+++ b40_cz/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 from won import views
 
