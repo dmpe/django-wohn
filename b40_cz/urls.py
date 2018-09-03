821e791cc33fa8427cecf9db6921c0a5205a7b76
John Malc <cincenko@outlook.com>
2018-09-03 17:53:48 +0200

    add robots and sitemap

--- b40_cz/urls.py
+++ b40_cz/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from won import views
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', include('won.urls')),
+
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file"),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
