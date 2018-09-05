2eaa525447d3b0a6e734c3e6790c74d9c2298229
John Malc <cincenko@outlook.com>
2018-09-05 17:17:21 +0200

    move to index

--- won/views.py
+++ won/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def user_profile(request):
     return render(request, 'user_profile.html')
