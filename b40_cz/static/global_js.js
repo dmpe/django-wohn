a43586aac6dfb020c9fa0c03d337460f6fb9bd41
John Malc <cincenko@outlook.com>
2018-09-07 17:44:35 +0200

    a

--- b40_cz/static/global_js.js
+++ b40_cz/static/global_js.js
@@ -1,11 +1,10 @@
 var open = 'fa-eye';
 var close = 'fa-eye-slash';
 var passwordInput = document.getElementById('inputPassword');
-var eye_outline = document.getElementById('inputGroupAppendButton');
-var icon = document.getElementById('inputGroupAppendEYE');
+var icon = document.getElementById('buttonEYE');
 
 // on click of the icon outline
-eye_outline.onclick = function() {
+icon.onclick = function() {
 	if(icon.classList.contains(open)) {
   	passwordInput.type="text";
     icon.classList.remove(open);
