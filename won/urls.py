90043725c3fc1325256bd203fe76ee6559c059c7
John Malc <cincenko@outlook.com>
2018-09-07 15:01:55 +0200

    add new apges

--- won/urls.py
+++ won/urls.py
@@ -1,11 +1,20 @@
+import views
+
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('user_profile', views.user_profile, name='user_profile'),
+    
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
     path('new_password', views.new_password, name='new_password'),
     path('reset_password', views.reset_password, name='reset_password'),
+    
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('terms', views.terms, name='terms'),
+    path('privacy', views.privacy, name='privacy'),
+    
 ]
--- won/views.py
+++ won/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 
-# Create your views here.
+# HOMEPAGE
 def index(request):
     return render(request, 'index.html')
 
+# ADMINISTRATION
 def user_profile(request):
     return render(request, 'user_profile.html')
 
+# HEADER - Main Body
 def register(request):
     return render(request, 'register.html')
 
@@ -20,3 +22,17 @@ def new_password(request):
 
 def reset_password(request):
 	return render(request, 'reset_password.html')
+
+
+# FOOTER
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def privacy(request):
+	return render(request, 'privacy.html')
+
+def terms(request):
+	return render(request, 'terms.html')
