76d942a00441188e97fa1c1883bc3110ac85d678
John Malc <cincenko@outlook.com>
2018-09-07 15:12:26 +0200

    tabs...

--- won/sitemap.py
+++ won/sitemap.py
@@ -5,15 +5,15 @@ from django.urls import reverse
 from datetime import *
 
 class B40_Sitemap(Sitemap):
-    """docstring for B40_Sitemap
-    """
-    changeFreq = "monthly"
-    priority = 0.6
+	"""docstring for B40_Sitemap
+	"""
+	changeFreq = "monthly"
+	priority = 0.6
 	lastmod = datetime.now()
 	protocol = "https"
 
-    def items(self):
-    	return ['index', 'contact', 'privacy', 'about', 'terms']
+	def items(self):
+		return ['index', 'contact', 'privacy', 'about', 'terms']
 
-    def location(self, item):
-    	return reverse(item)
+	def location(self, item):
+		return reverse(item)
