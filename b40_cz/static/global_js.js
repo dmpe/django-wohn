38a7ecaa481ccb4146820033b615df5db1e66cd9
John Malc <cincenko@outlook.com>
2018-09-07 17:31:27 +0200

    add new js button toggler

---
+++ b40_cz/static/global_js.js
@@ -0,0 +1,18 @@
+var open = 'fa-eye';
+var close = 'fa-eye-slash';
+var passwordInput = document.getElementById('inputPassword');
+var eye_outline = document.getElementById('inputGroupAppend');
+var icon = document.getElementById('inputGroupAppendEYE');
+
+// on click of the icon outline
+eye_outline.onclick = function() {
+	if(icon.classList.contains(open)) {
+  	passwordInput.type="text";
+    icon.classList.remove(open);
+    icon.className += ' '+ close;
+  } else {
+  	passwordInput.type="password";
+    icon.classList.remove(close);
+    icon.className += ' '+ open;
+  }
+}
