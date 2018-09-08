35b3b9402e1bff924b8ecb997ca0f759d39d32a5
John Malc <cincenko@outlook.com>
2018-09-08 17:54:54 +0200

    ss

--- b40_cz/settings.py
+++ b40_cz/settings.py
@@ -136,7 +136,9 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/css/'),
+    os.path.join(PROJECT_ROOT, 'static/images/'),
+    os.path.join(PROJECT_ROOT, 'static/js/'),
 ]
 
 # Simplified static file serving.
