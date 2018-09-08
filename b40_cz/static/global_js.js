2c3748f07b5cba3d3485bc7fab0f63808a40f9ed
John Malc <cincenko@outlook.com>
2018-09-08 15:16:31 +0200

    as

--- b40_cz/static/global_js.js
+++ b40_cz/static/global_js.js
@@ -9,41 +9,45 @@ var icon = document.getElementById('buttonEYE');
 var icon2 = document.getElementById('buttonEYE2');
 var icon3 = document.getElementById('buttonEYE3');
 
-// on click of the icon outline
-icon.onclick = function() {
-	if(icon.classList.contains(open)) {
-  	passwordInput.type="text";
-    icon.classList.remove(open);
-    icon.className += ' '+ close;
-  } else {
-  	passwordInput.type="password";
-    icon.classList.remove(close);
-    icon.className += ' '+ open;
-  }
+if (typeof(icon) != 'undefined' && icon != null) {
+	// on click of the icon outline
+	icon.onclick = function() {
+		if(icon.classList.contains(open)) {
+	  	passwordInput.type="text";
+	    icon.classList.remove(open);
+	    icon.className += ' '+ close;
+	  } else {
+	  	passwordInput.type="password";
+	    icon.classList.remove(close);
+	    icon.className += ' '+ open;
+	  }
+	}	
 }
-
-icon2.onclick = function() {
-	if(icon2.classList.contains(open)) {
-  	passwordNewInput.type="text";
-    icon2.classList.remove(open);
-    icon2.className += ' '+ close;
-  } else {
-  	passwordNewInput.type="password";
-    icon2.classList.remove(close);
-    icon2.className += ' '+ open;
-  }
+if (typeof(icon2) != 'undefined' && icon2 != null) {
+	icon2.onclick = function() {
+		if(icon2.classList.contains(open)) {
+	  	passwordNewInput.type="text";
+	    icon2.classList.remove(open);
+	    icon2.className += ' '+ close;
+	  } else {
+	  	passwordNewInput.type="password";
+	    icon2.classList.remove(close);
+	    icon2.className += ' '+ open;
+	  }
+	}
 }
-
-icon3.onclick = function() {
-	if(icon3.classList.contains(open)) {
-  	passwordConfirmNewInput.type="text";
-    icon3.classList.remove(open);
-    icon3.className += ' '+ close;
-  } else {
-  	passwordConfirmNewInput.type="password";
-    icon3.classList.remove(close);
-    icon3.className += ' '+ open;
-  }
+if (typeof(icon3) != 'undefined' && icon3 != null) {
+	icon3.onclick = function() {
+		if(icon3.classList.contains(open)) {
+	  	passwordConfirmNewInput.type="text";
+	    icon3.classList.remove(open);
+	    icon3.className += ' '+ close;
+	  } else {
+	  	passwordConfirmNewInput.type="password";
+	    icon3.classList.remove(close);
+	    icon3.className += ' '+ open;
+	  }
+	}
 }
 
 $("#emailSendAlert").click(function(){
