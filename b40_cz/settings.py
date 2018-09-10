7c185f71252186e43a21f7e6b55f8e6f77cc4cdc
John Malc <cincenko@outlook.com>
2018-09-10 14:55:32 +0200

    as

--- b40_cz/settings.py
+++ b40_cz/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'b40_cz.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 # Change 'default' database configuration with $DATABASE_URL.
 DATABASES = {}
+DATABASES['default'].config(default=os.environ.get('DATABASE_URL'))
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 AUTH_PASSWORD_VALIDATORS = [
