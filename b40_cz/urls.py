265e95bb44622c95ed30c9eab7d71f9ab5c01831
John Malc <cincenko@outlook.com>
2018-09-17 15:26:24 +0200

    asf

--- b40_cz/urls.py
+++ b40_cz/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls import url
 from core import views
 from core.sitemap import B40_Sitemap
 
-from user_management import views
+from userManagement import views
 
 sitemaps = {
     'static': B40_Sitemap,
@@ -33,7 +33,7 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', include('core.urls')),
-    path('', include('user_management.urls')),
+    path('', include('userManagement.urls')),
 
     url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file"),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
--- userManagement/urls.py
+++ userManagement/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include
 urlpatterns = [  
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('logout', views.login, name='logout'),
+    path('logout', views.logout, name='logout'),
     path('new_password', views.new_password, name='new_password'),
     path('reset_password', views.reset_password, name='reset_password'),
     path('user_profile', views.user_profile, name='user_profile'),
