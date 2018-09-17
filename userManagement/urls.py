519473ad752acb8f904c2510d3deea353fe0462c
John Malc <cincenko@outlook.com>
2018-09-17 17:26:21 +0200

    hj

--- userManagement/urls.py
+++ userManagement/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('new_password', views.new_password, name='new_password'),
     path('reset_password', views.reset_password, name='reset_password'),
-    path('user_profile', views.user_profile, name='user_profile'),
-    path('index', views.index, name='index'),
+    
+    path('/administrace/user_profile', views.user_profile, name='user_profile'),
+    path('/administrace/', views.index, name='index'),
+    
     path('oauth/', include('social_django.urls', namespace='social'))    
 ]
