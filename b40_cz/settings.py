3aca21d9bb42eff375994222a3c99bdbc38574e4
John Malc <cincenko@outlook.com>
2018-09-17 17:34:36 +0200

    bu

--- b40_cz/settings.py
+++ b40_cz/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 import django_heroku
 
 SOCIAL_AUTH_USER_MODEL = 'userManagement.MyUser'
-#AUTH_USER_MODEL = 'userManagement.MyUser'
+AUTH_USER_MODEL = 'userManagement.MyUser'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- userManagement/urls.py
+++ userManagement/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('new_password', views.new_password, name='new_password'),
     path('reset_password', views.reset_password, name='reset_password'),
     
-    path('/administrace/user_profile', views.user_profile, name='user_profile'),
-    path('/administrace/', views.index, name='index'),
+    path('administrace/user_profile', views.user_profile, name='user_profile'),
+    path('administrace/', views.index, name='index'),
     
-    path('oauth/', include('social_django.urls', namespace='social'))    
+    path('/oauth/', include('social_django.urls', namespace='social'))    
 ]
