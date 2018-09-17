9d30419b4bd0be6388668bd09cdc3662db04d2e7
John Malc <cincenko@outlook.com>
2018-09-17 15:47:30 +0200

    sdf

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class ApartmentType(models.Model):
 	"""
 	Define each apartment, 1-to-n with Users
 	"""
-	apartment_offered_by = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+	apartment_offered_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	apartment_rooms = models.IntegerField()
 	apartment_created = models.DateTimeField(auto_now_add=True)
 	# https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=13.04.2018
