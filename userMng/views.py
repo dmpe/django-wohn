4f97b7671c8bd88e0c50ef3b0ec347c0255ab748
John Malc <cincenko@outlook.com>
2018-09-21 17:22:58 +0200

    change logic in header and disable for the moment login logic

--- userMng/views.py
+++ userMng/views.py
@@ -16,20 +16,20 @@ def register(request):
     return render(request, 'register.html')
 
 def login(request):
-	render(request, 'login.html')
+	return render(request, 'login.html')
 
-	if 'usernameOrEmail' and 'user_password' in request.POST:
-		usernameOrEmail = request.POST['inputEmail_Username']
-		user_password = request.POST['inputNewPassword']
-	else:
-		usernameOrEmail = False
-		user_password = False
+	# if 'usernameOrEmail' and 'user_password' in request.POST:
+	# 	usernameOrEmail = request.POST['inputEmail_Username']
+	# 	user_password = request.POST['inputNewPassword']
+	# else:
+	# 	usernameOrEmail = False
+	# 	user_password = False
 
-	auth_user = authenticate(request, username = usernameOrEmail, password = user_password)
+	# auth_user = authenticate(request, username = usernameOrEmail, password = user_password)
 
-	if auth_user is not None:
-		login(request, auth_user)
-		return auth_user
+	# if auth_user is not None:
+	# 	login(request, auth_user)
+	# 	return auth_user
 
 def new_password(request):
 	return render(request, 'new_password.html')
