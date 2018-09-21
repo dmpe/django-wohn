431dad37d4c8c3ce7d52b7f62f8dfb086eb365ce
John Malc <cincenko@outlook.com>
2018-09-21 17:40:02 +0200

    again logout functionality attempt

--- userMng/views.py
+++ userMng/views.py
@@ -38,6 +38,6 @@ def reset_password(request):
 	return render(request, 'reset_password.html')
 
 def logout(request):
-	#redirect('logout.html')
-	return logout(request)
-	#return redirect('core_index')
+	#render(request,'logout.html')
+	logout(request)
+	redirect('core_index')
