5d7ebd7c7d4b29f5098f0336889c112f9b532226
John Malc <cincenko@outlook.com>
2018-09-21 16:59:27 +0200

    still wrong argument

--- userMng/urls.py
+++ userMng/urls.py
@@ -5,10 +5,11 @@ from django.conf import settings
 
 #app_name = 'userMng'
 # https://stackoverflow.com/questions/49655525/django-2-0-not-a-valid-view-function-or-pattern-name-customizing-auth-views
+
 urlpatterns = [  
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    path('logout', views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    path('logout', views.logout, name='logout'),
     path('new_password', views.new_password, name='new_password'),
     path('reset_password', views.reset_password, name='reset_password'),
     
