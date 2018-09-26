3b8325c5db931a104ab3ab26f6d8fe8fd59d3771
John Malc <cincenko@outlook.com>
2018-09-26 23:13:57 +0200

    class based approach is currently broken the broad idea

--- userMng/views.py
+++ userMng/views.py
@@ -4,6 +4,7 @@ from django import forms
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth import login as django_login
 from django.views import View
+from django.conf import settings
 
 # a generic view for creating and saving an object (e.g. user)
 from django.views.generic.edit import CreateView
@@ -23,8 +24,8 @@ class RegistrationView(CreateView):
 	"""
 	template_name = 'signup_login/register.html'
 
-	#def register(request):
-	#	return render(request, 'register.html')
+	def get(self, request):
+		return render(request, self.template_name)
 
 class LoginView(View):
 	"""Uses class based view
@@ -34,18 +35,20 @@ class LoginView(View):
 	def post(self, request):
 		return render(request, 'login.html')
 
-	# if 'usernameOrEmail' and 'user_password' in request.POST:
-	# 	usernameOrEmail = request.POST['inputEmail_Username']
-	# 	user_password = request.POST['inputNewPassword']
-	# else:
-	# 	usernameOrEmail = False
-	# 	user_password = False
+	if 'usernameOrEmail' and 'user_password' in request.POST:
+	 	usernameOrEmail = request.POST['inputEmail_Username']
+	 	user_password = request.POST['inputNewPassword']
+	else:
+	 	usernameOrEmail = False
+	 	user_password = False
 
-	# auth_user = authenticate(request, username = usernameOrEmail, password = user_password)
+	auth_user = authenticate(request, username = usernameOrEmail, password = user_password)
 
-	# if auth_user is not None:
-	# 	django_login(request, auth_user)
-	# 	return auth_user
+	if auth_user is not None:
+	 	django_login(request, auth_user)
+	 	return auth_user
+	else:
+		return redirect(settings.LOGIN_URL)
 
 def new_password(request):
 	return render(request, 'new_password.html')
@@ -56,11 +59,11 @@ def reset_password(request):
 class LogoutView(View):
 	"""class based view for logout
 	"""
-	template_name = 'logout.html'
+	#template_name = 'signup_login/logout.html'
 	# redirect to logout.html page, which again rederects to core_index
-	#render(request,'logout.html')
 		
 	def get(self, request):
 		# enable to make first move to logout page and then user logout
 		django_logout(request)
+		render(request,'logout.html')
 		return redirect('core_index')
