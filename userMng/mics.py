a0c5265a13c7d107f310143faeec4af27cadb423
John Malc <cincenko@outlook.com>
2018-10-14 15:51:40 +0200

    is_human function causes not being able to login add more debugging

--- userMng/mics.py
+++ userMng/mics.py
@@ -76,11 +76,12 @@ def is_human(recaptcha_token = None):
 	Used for token verification. 
 	Borrowed from https://techmonger.github.io/5/python-flask-recaptcha/
 	"""
+	print(recaptcha_token)
 	payload = {'response':recaptcha_token, 'secret':settings.GOOGLE_RECAPTCHA_V3}
 	response_ggl = requests_library.post("https://www.google.com/recaptcha/api/siteverify", payload)
 	response_text = json.loads(response_ggl.text)
 	print(response_text["success"])
-	print(response_text["score"])
+	print(response_text)
 
 	if response_text["success"] is True and response_text["score"] >= 0.1:
 		return True
