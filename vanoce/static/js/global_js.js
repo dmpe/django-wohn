22abf432d9293c60b1b60d6997bc2be229e62076
f789gh <f789gh@bk.ru>
2018-10-22 21:16:29 +0200

    adjust javascript function

--- vanoce/static/js/global_js.js
+++ vanoce/static/js/global_js.js
@@ -11,34 +11,37 @@ var icon = document.getElementById('buttonEYE');
 var icon2 = document.getElementById('buttonEYE2');
 var icon3 = document.getElementById('buttonEYE3');
 
+
 if (typeof(icon) != 'undefined' && icon != null) {
 	// if not null, then on click of the icon replace types & add/remove icon CSS classes
+
 	icon.onclick = function() {
 		if(icon.classList.contains(open)) {
-	  	passwordInput.type="text";
-	    icon.classList.remove(open);
-	    icon.className += ' '+ close;
-	  } else {
-	  	passwordInput.type="password";
-	    icon.classList.remove(close);
-	    icon.className += ' '+ open;
-	  }
+		  	passwordInput.type="text";
+		    icon.classList.remove(open);
+		    icon.className += ' '+ close;
+		} else {
+		  	passwordInput.type="password";
+		    icon.classList.remove(close);
+		    icon.className += ' '+ open;
+		}
 	}	
 }
 
-// if (typeof(icon2) != 'undefined' && icon2 != null) {
-// 	icon2.onclick = function() {
-// 		if(icon2.classList.contains(open)) {
-// 	  	passwordNewInput.type="text";
-// 	    icon2.classList.remove(open);
-// 	    icon2.className += ' '+ close;
-// 	  } else {
-// 	  	passwordNewInput.type="password";
-// 	    icon2.classList.remove(close);
-// 	    icon2.className += ' '+ open;
-// 	  }
-// 	}
-// }
+if (typeof(icon2) != 'undefined' && icon2 != null) {
+	var svg_icon = document.querySelector(CSS.escape(icon2) + "> svg:first-of-type");
+	icon2.onclick = function() {
+		if(svg_icon.classList.contains(open)) {
+	  	passwordNewInput.type="text";
+	    svg_icon.classList.remove(open);
+	    svg_icon.className += ' ' + close;
+	  } else {
+	  	passwordNewInput.type="password";
+	    svg_icon.classList.remove(close);
+	    svg_icon.className += ' '+ open;
+	  }
+	}
+}
 
 if (typeof(icon3) != 'undefined' && icon3 != null) {
 	icon3.onclick = function() {
