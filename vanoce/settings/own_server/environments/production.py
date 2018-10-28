623decb8d1b821425122ebf01e2be3d9a43d58cf
f789gh <f789gh@bk.ru>
2018-10-28 14:42:03 +0100

    wrong user most probably

--- vanoce/settings/own_server/environments/production.py
+++ vanoce/settings/own_server/environments/production.py
@@ -44,7 +44,7 @@ DATABASES = {
    'default': {
 	   'ENGINE': 'django.db.backends.postgresql_psycopg2',
 	   'NAME': client.get_secret("https://b40.vault.azure.net/", "DATABASE-NAME", "76f97796b24a4833a467a2bb1ce64780").value,
-	   'USER': client.get_secret("https://b40.vault.azure.net/", "DATABASE-USER", "6dde773db1e3474bae4405cef2ae2ea6").value,
+	   'USER': client.get_secret("https://b40.vault.azure.net/", "DATABASE-USER", "6f6c1f6668ae45c0b85c54c1526d8d45").value,
 	   'PASSWORD': client.get_secret("https://b40.vault.azure.net/", "DATABASE-PASSWORD", "34b5500d04f84f28ba64772d81aa4cb2").value,
 	   'HOST': client.get_secret("https://b40.vault.azure.net/", "DATABASE-HOST", "4c6a2e23959349a6a8021387957efcdc").value,
 	   'PORT': '5432',
