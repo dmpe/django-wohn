362c1c40170f741ab87af76e1fb4e0c607cc12ed
f789gh <f789gh@bk.ru>
2018-10-28 15:10:30 +0100

    maybe like this with slash ?

--- vanoce/settings/own_server/components/common.py
+++ vanoce/settings/own_server/components/common.py
@@ -204,7 +204,7 @@ MEDIA_URL = 'https://djangowohnreal1.blob.core.windows.net/%s/' % AZURE_CONTAINE
 # Static files (CSS, JavaScript, Images)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static/css/'),
