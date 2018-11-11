b0c936dd39b9e0f08f9d950632637966644a838c
f789gh <f789gh@bk.ru>
2018-11-11 12:01:29 +0100

    additional sorting

--- core/admin.py
+++ core/admin.py
@@ -68,7 +68,7 @@ class ExchangeRateAdmin(admin.ModelAdmin):
 
 		# convert to pandas
 		two_col_df = pd.DataFrame.from_records(two_col, columns = ['today', currency])
-		two_col_df.sort_values(by = ['today'])
+		two_col_df.sort_values(by = ['today'], ascending=True )
 
 		print('we are in prepare_data func: df ->', two_col_df)
 		# export to json object - to try...
