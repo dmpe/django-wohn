e4f3f318d5b511f5e740778047148d4aa7bc8764
dmpe <cincenko@outlook.com>
2018-11-15 19:33:55 +0000

    change order of uniqueness

--- userMng/views.py
+++ userMng/views.py
@@ -166,9 +166,9 @@ class RegistrationView(CreateView):
 				ur.save()
 			except (IntegrityError, ValidationError) as e:
 				messages.add_message(request, messages.WARNING, 
-					mark_safe('<h6 class=''alert-heading''>Username or Email already exist</h6>'
+					format_html('<h6 class=''alert-heading''>Username or Email already exist</h6>'
 					'<p>It seems that your username and/or email already exist in your system.</p>'
-					'<p>You can reset <a class = ''alert-link'' href="https:/b40re.tk/reset_password">your password</a> or provide again a unique combination of username & email.</p>'))
+					'<p>You can reset <a class = ''alert-link'' href="{}">your password</a> or provide again a unique combination of username & email.</p>'), reverse('reset_password'))
 				
 				return render(request, self.template_name)
 		else: 
