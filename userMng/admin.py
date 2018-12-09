62f816480fbd49f9cea4d578dbbae4c5fc5027de
f789gh <f789gh@bk.ru>
2018-12-09 18:42:53 +0100

    wrong, start from begenning

--- userMng/admin.py
+++ userMng/admin.py
@@ -9,40 +9,24 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import myUser
 
 
-class UserCreationForm(forms.ModelForm):
-	"""docstring for UserCreationForm"""
-	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+# class UserCreateForm(UserCreationForm):
+# 	"""docstring for UserCreationForm"""
 
-	class Meta:
-		model = myUser
-		fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
+# 	class Meta:
+# 		model = myUser
+# 		fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
 	
-	def clean_password2(self):
-		password1 = self.cleaned_data.get("password1")
-		password2 = self.cleaned_data.get("password2")
-		if password1 and password2 and password1 != password2:
-			raise forms.ValidationError("Passwords don't match")
-		return password2
-
-	def save(self, commit=True):
-		user = super().save(commit=False)
-		user.set_password(self.cleaned_data["password1"])
-		if commit:
-			user.save()
-		return user
-
-class NewUserChangeForm(forms.ModelForm):
-
-	class Meta:
-		model = myUser
-		fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
+# 	def save(self, commit=True):
+# 		user = super(UserCreateForm, self).save(commit=False)
+# 		user
+# 		if commit:
+# 			user.save()
+# 		return user
 
 class UserAdmin(BaseUserAdmin):
-	add_form = UserCreationForm
-	form = NewUserChangeForm
+	# add_form = UserCreateForm
 
-	# list_display = ('username', 'user_timezone', 'user_int_tel', 'first_name', 'last_name', 'last_login',)
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'user_timezone', 'user_int_tel')
 	
 	fieldsets = (
 		(None, {'fields': ('username', 'password1')}),
