11b4f02ca400479b49cd4572ec33f21ad064ec44
f789gh <f789gh@bk.ru>
2018-12-09 17:45:27 +0100

    prepare new usercreation form

--- userMng/admin.py
+++ userMng/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 from .models import myUser
 
+
+# class UserCreationForm(forms.ModelForm):
+# 	"""docstring for UserCreationForm"""
+# 	model = myUser
+# 	fields = ('user_timezone', 'user_int_tel')
+
+class UserAdmin(BaseUserAdmin):
+	model = myUser
+	filter_horizontal = ('user_timezone', 'user_int_tel')
+	
 admin.site.register(myUser, UserAdmin)
