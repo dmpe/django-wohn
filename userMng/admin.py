9707ada182d81a3f71f17ab8563b782e8d2a04b4
f789gh <f789gh@bk.ru>
2018-12-09 18:03:09 +0100

    do it like in the documentation

--- userMng/admin.py
+++ userMng/admin.py
@@ -11,16 +11,39 @@ from .models import myUser
 
 class UserCreationForm(forms.ModelForm):
 	"""docstring for UserCreationForm"""
+	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
 	class Meta:
 		model = myUser
-		fields = '__all__'
+		fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
+	
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("Passwords don't match")
+		return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class NewUserChangeForm(forms.ModelForm):
+
+    class Meta:
+        model = myUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
 
 class UserAdmin(BaseUserAdmin):
 	add_form = UserCreationForm
 	add_fieldsets = (
 		('None', {
 			'classes': ('wide',),
-			'fields': ('user_timezone', 'user_int_tel')}
+			'fields': ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel')}
 		), 
 	)
 
