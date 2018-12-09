4c8858bfe96e904a68086dc19909c3214623c5d8
f789gh <f789gh@bk.ru>
2018-12-09 19:00:25 +0100

    old school approach. copy paste from source code

--- userMng/admin.py
+++ userMng/admin.py
@@ -27,8 +27,14 @@ class UserAdmin(BaseUserAdmin):
 	# add_form = UserCreateForm
 	class Meta:
 		user = myUser
-		fields = ('user_timezone', 'user_int_tel')
-		
+
+	fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'user_timezone', 'user_int_tel')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')})
+    )
+
 	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'user_timezone', 'user_int_tel')
 	
 	# fieldsets = (
