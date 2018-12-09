ce3b4b36e2899eaf6fd141fbe4b4043b08bf8d41
f789gh <f789gh@bk.ru>
2018-12-09 19:04:27 +0100

    add email as well

--- userMng/admin.py
+++ userMng/admin.py
@@ -8,23 +8,9 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 from .models import myUser
 
-
-# class UserCreateForm(UserCreationForm):
-# 	"""docstring for UserCreationForm"""
-
-# 	class Meta:
-# 		model = myUser
-# 		fields = ('username', 'email', 'first_name', 'last_name', 'user_timezone', 'user_int_tel') 
-	
-# 	def save(self, commit=True):
-# 		user = super(UserCreateForm, self).save(commit=False)
-# 		user
-# 		if commit:
-# 			user.save()
-# 		return user
-
 class UserAdmin(BaseUserAdmin):
 	# add_form = UserCreateForm
+
 	class Meta:
 		user = myUser
 
@@ -36,13 +22,12 @@ class UserAdmin(BaseUserAdmin):
     )
 
 	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'user_timezone', 'user_int_tel')
-	
-	# fieldsets = (
-	# 	(None, {'fields': ('username', 'password')}),
-	# 	('Personal info', {'fields': ('first_name', 'last_name', 'user_timezone', 'user_int_tel',)})
-	# )
-
-	
-	
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password', 'email')
+        })
+    )
 
 admin.site.register(myUser, UserAdmin)
