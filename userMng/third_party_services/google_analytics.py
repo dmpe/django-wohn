2afd443c362f1e994ffb292fc147792cbffe112a
dmpe <cincenko@outlook.com>
2018-12-12 23:09:55 +0000

    still the same stuff of loading ga python class #13

--- userMng/third_party_services/google_analytics.py
+++ userMng/third_party_services/google_analytics.py
@@ -18,7 +18,8 @@ class Google_Analytics:
 		"""
 		SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 		KEY_FILE_LOCATION = 'client_secrets.json'
-		credentials = ServiceAccountCredentials.from_json_keyfile_name(self.KEY_FILE_LOCATION, self.SCOPES)
+		
+		credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_LOCATION, SCOPES)
 	
 		# Build the service object.
 		analytics = build('analyticsreporting', 'v4', credentials=credentials)
@@ -41,7 +42,7 @@ class Google_Analytics:
 				body={
 					'reportRequests': [
 					{
-						'viewId': self.VIEW_ID,
+						'viewId': VIEW_ID,
 						'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
 						'metrics': [{'expression': 'ga:sessions', "expression":"ga:pageviews"}],
 						'dimensions': [{'name': 'ga:country', "name":"ga:browser"}]
