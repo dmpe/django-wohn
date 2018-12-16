cc59b98e7517db006d02901af4a288f34df6894d
f789gh <f789gh@bk.ru>
2018-12-16 20:25:27 +0100

    get rid of core_index, renamed to homepage add country to admin page

--- userMng/admin.py
+++ userMng/admin.py
@@ -41,7 +41,7 @@ class UserAdmin(BaseUserAdmin):
 
 	fieldsets = (
 		(None, {'fields': ('username', 'password')}),
-		('Personal info', {'fields': ('first_name', 'last_name', 'email', 'user_timezone', 'user_int_tel')}),
+		('Personal info', {'fields': ('first_name', 'last_name', 'email', 'user_timezone', 'user_int_tel', 'country')}),
 		('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
 		('Important dates', {'fields': ('last_login', 'date_joined')})
 	)
