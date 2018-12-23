c34fb34d28bc98b8a5244f29894f59df094e473f
f789gh <f789gh@bk.ru>
2018-12-23 16:37:50 +0100

    variable versus function https://stackoverflow.com/questions/44135225/attributeerror-logger-object-has-no-attribute-warning

--- userMng/views.py
+++ userMng/views.py
@@ -303,7 +303,7 @@ class LoginView(View):
 		username_email = request.POST.get('inputEmail_Username', False)
 		user_password = request.POST.get('inputNewPassword', False)
 		recap_token = request.POST.get('g-recaptcha-response', False)
-		logger.INFO(recap_token)
+		logger.info(recap_token)
 
 		if is_human(recap_token):
 			try:
