eb31cab226e2d96f86032a8386ddbf083f38a793
f789gh <f789gh@bk.ru>
2018-12-30 16:42:11 +0100

    variety of smaller frondend and backend improvements

--- vanoce/celery.py
+++ vanoce/celery.py
@@ -5,9 +5,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanoce.settings')
 
-app = Celery('vanoce',
-             backend='amqp',
-             broker='amqp://localhost//')
+app = Celery('vanoce', backend='amqp', broker='amqp://localhost//')
 
 # This reads, e.g., CELERY_ACCEPT_CONTENT = ['json'] from settings.py:
 app.config_from_object('django.conf:settings')
@@ -15,16 +13,15 @@ app.config_from_object('django.conf:settings')
 # For autodiscover_tasks to work, you must define your tasks in a file called 'tasks.py'.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
 @app.task(bind=True)
 def debug_task(self):
 	print("Request: {0!r}".format(self.request))
 
 
 app.conf.update(
-    result_expires=3600,
-    timezone = 'Europe/Prague'
+	result_expires=3600,
+	timezone = 'Europe/Prague'
 )
 
 if __name__ == '__main__':
-    app.start()
+	app.start()
