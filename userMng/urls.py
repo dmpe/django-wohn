9e1514f8b5e4bbb0dc10371d9551dc5f2a7e6ae3
John Malc <cincenko@outlook.com>
2019-03-22 18:07:34 +0100

    delete for the moment pinax messages

--- userMng/urls.py
+++ userMng/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
 	path('administrace/', views.UserProfileIndex.as_view(), name='userMng_index'),    
     path('administrace/profile', views.UserProfileAdministration.as_view(), name='user_profile'), 
     path('administrace/properties', views.UserProfileProperties.as_view(), name='user_properties'),
-    path('administrace/messages/', include("pinax.messages.urls", namespace="pinax_messages")),
+    # path('administrace/messages/', include("pinax.messages.urls", namespace="pinax_messages")),
 
     path('administrace/my_property_ads', views.AdvertisingMyAdd.as_view(), name='my_property_ads'),
     path('administrace/ad_statistics', views.AdvertisingStatistics.as_view(), name='ad_statistics'),
