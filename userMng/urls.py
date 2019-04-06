b586750370f0aa5fccad449d6808bae1548c0f66
John Malc <cincenko@outlook.com>
2019-04-06 20:29:04 +0200

    update header

--- userMng/urls.py
+++ userMng/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include
 from django.conf import settings
 from pinax.messages import *
 
-# app_name = 'userMng'
+app_name = 'userMng'
 # https://stackoverflow.com/questions/49655525/django-2-0-not-a-valid-view-function-or-pattern-name-customizing-auth-views
 
 urlpatterns = [   
