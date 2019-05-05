fcfd5864c67648ec2d57a226ce54eec1573e0b34
dmpe <cincenko@outlook.com>
2019-05-05 15:27:16 +0200

    add connection string to key vault

--- vanoce/celery.py
+++ vanoce/celery.py
@@ -3,9 +3,18 @@ import os
 from django.conf import settings
 from celery import Celery
 
+# for Azure Key Vault
+from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+from azure.common.credentials import ServicePrincipalCredentials
+from msrestazure.azure_active_directory import MSIAuthentication
+credentials = MSIAuthentication(resource='https://vault.azure.net')
+client = KeyVaultClient(credentials)
+
+BLOB_STORAGE_CON_STRING = client.get_secret("https://b40.vault.azure.net/", "AZURE-STORAGE-KEY-CON-STRING", "f74b5febb5e649fba53e7222a7e29d92").value
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanoce.settings')
 
-app = Celery('vanoce', backend='rpc', broker='amqp://jm:159753@localhost//')
+app = Celery('vanoce', azureblockblob_container_name = "celery", backend=BLOB_STORAGE_CON_STRING, broker='amqp://jm:159753@localhost//')
 
 # This reads, e.g., CELERY_ACCEPT_CONTENT = ['json'] from settings.py:
 app.config_from_object('django.conf:settings')
