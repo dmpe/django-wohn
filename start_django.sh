5447849d313fdbc51b5d60ddb9ae5064503434c4
dmpe <cincenko@outlook.com>
2019-08-17 01:03:46 +0200

    stage [skip ci]

--- start_django.sh
+++ start_django.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -ex
 
@@ -6,4 +6,5 @@ python3 manage.py collectstatic
 python3 manage.py makemigrations core 
 python3 manage.py makemigrations userMng
 python3 manage.py migrate
+python3 manage.py graph_models -a -g -o output/class_diagramm.png
 gunicorn vanoce.wsgi:application --log-level=info -b 127.0.0.1:8123 --workers 3
