21a8af14efbf4b678fd0f06e9cf494a5a9ebec33
dmpe <cincenko@outlook.com>
2019-08-20 22:24:22 +0200

    add more vs code settings and replace old domain name

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -8,13 +8,25 @@ credentials = MSIAuthentication(resource='https://vault.azure.net')
 client = KeyVaultClient(credentials)
 
 class AzureMediaStorage(AzureStorage):
-	account_name = 'melivexyz5555'
-	account_key = client.get_secret("https://b40.vault.azure.net/", "AZURE-ACCOUNT-KEY", "36456572c12640afa4c2ba448169ee66").value
-	azure_container = 'images-profile-pictures'
-	expiration_secs = None
+    """ Replace with Azure Storage Account data
+    
+    Used for media files. 
+
+    The account key becomes a secret, fetched from Azure Key Vault
+    """
+    account_name = 'melivexyz5555'
+    account_key = client.get_secret("https://b40.vault.azure.net/", "AZURE-ACCOUNT-KEY", 
+                                    "36456572c12640afa4c2ba448169ee66").value
+    azure_container = 'images-profile-pictures'
+    expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-	account_name = 'melivexyz5555' # Must be replaced by your storage_account_name
-	account_key = client.get_secret("https://b40.vault.azure.net/", "AZURE-ACCOUNT-KEY", "36456572c12640afa4c2ba448169ee66").value
-	azure_container = 'static'
-	expiration_secs = None
+    """ Same as previous class
+    
+    Used for statics files, e.g. css, js, etc.
+    """
+    account_name = 'melivexyz5555' 
+    account_key = client.get_secret("https://b40.vault.azure.net/", "AZURE-ACCOUNT-KEY", 
+                                    "36456572c12640afa4c2ba448169ee66").value
+    azure_container = 'static'
+    expiration_secs = None
