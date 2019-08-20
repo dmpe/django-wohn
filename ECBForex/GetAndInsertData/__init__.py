d8cb2e2f35b3d80e390f26ca37765e89d60a939d
dmpe <cincenko@outlook.com>
2019-08-21 00:35:02 +0200

    prepare first function

---
+++ ECBForex/GetAndInsertData/__init__.py
@@ -0,0 +1,86 @@
+import datetime
+import logging
+import pickle
+
+import azure.functions as func
+
+import numpy as np
+# import for (forex) data processing
+import pandas as pd
+from pandasdmx import *
+from pandasdmx.api import *
+
+# def parse_forex_data(mytimer2: func.TimerRequest) -> None:
+#     """
+# 	Runs every month
+# 	https://stackoverflow.com/a/38286238
+# 	https://realpython.com/asynchronous-tasks-with-django-and-celery/
+# 	"""
+# 	# {EUR, USD} <> CZK
+# 	csob_forex = "https://www.csob.cz/portal/lide/kurzovni-listek/-/date/kurzy.txt"
+	
+# 	data = pd.read_csv(csob_forex, delimiter = ";", skiprows=3, encoding="utf-8", thousands='.', decimal=',')
+# 	data = data.rename({"Měna": 'currency', "Střed.1":'exchange_rate_czk'}, axis='columns')
+	
+# 	Oneeur_czk = data[data['currency'].isin(["EUR", "USD"])].iloc[0]['exchange_rate_czk']
+# 	Oneusd_czk = data[data['currency'].isin(["EUR", "USD"])].iloc[1]['exchange_rate_czk']
+# 	Oneeur_usd = None
+
+# 	# used for ECB request and exchange_dict if necessary
+# 	current_date = datetime.datetime.utcnow().replace(tzinfo=utc)
+
+# 	# 1 EUR <> USD
+# 	ecb = Request('ECB')
+
+# 	try:
+# 		exr_flow = ecb.data(resource_id = 'EXR', key={'CURRENCY': 'USD'}, params = {'startPeriod': str(current_date.year)})
+# 		daily = (s for s in exr_flow.data.series if s.key.FREQ == 'D')
+
+# 		# write to pandas dataset, first the the top
+# 		cur_df = exr_flow.write(daily)
+# 		Oneeur_usd = np.array(cur_df.iloc[[-1]]).item(0)
+		
+# 	except SDMXException as e:
+# 		# server maintanance - e.g. error 500
+# 		print(e)
+# 		logger.WARNING(e)
+	
+# 	if (Oneeur_usd is not None):
+# 		# selected 
+# 		exchange_dict = {
+# 			# 'date': current_date,
+# 			'1eur_czk': Oneeur_czk,
+# 			'1usd_czk': Oneusd_czk,
+# 			'1eur_usd': Oneeur_usd
+# 		}
+
+# 		# Pickling to file system
+# 		cur_list = [(k,v) for k,v in exchange_dict.items()]
+# 		with open("currency_list_pickle", "wb") as fp:   
+# 			pickle.dump(cur_list, fp)
+		
+# 		# write our data to the database model so that we can query it in the 
+# 		# admin page and show highstock chart
+# 		# forex_model = ExchangeRate()
+# 		# forex_model.OneEurCzk = exchange_dict['1eur_czk']
+# 		# forex_model.OneUsdCzk = exchange_dict['1usd_czk']
+# 		# forex_model.OneEurUsd = exchange_dict['1eur_usd']
+# 		# forex_model.save()
+# 		return exchange_dict
+	
+# 	else:
+# 		print("var Oneeur_usd is empty/null because connection to the server could not have been established")
+# 		return None
+
+# 	# write results to a file txt -> in production read from the file
+# 	# with open("test.txt", "rb") as fp:   # Unpickling
+# 		# b = pickle.load(fp)
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
