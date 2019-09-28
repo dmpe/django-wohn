85b34673273b6c769747f15054ccdaeb5a53195e
John Malc <cincenko@outlook.com>
2019-09-29 01:06:43 +0200

    all ips

--- start_django.sh
+++ start_django.sh
@@ -8,4 +8,4 @@ python3 manage.py makemigrations core
 python3 manage.py makemigrations userMng
 python3 manage.py migrate
 python3 manage.py graph_models -a -g -o output/class_diagramm.png
-gunicorn melive.wsgi:application --log-level=info -b 127.0.0.1:8123 --workers 3
+gunicorn melive.wsgi:application --log-level=info -b :8123 --workers 3
