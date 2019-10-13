abbe75c6da1705103fee7d991880dd4fd49669c2
John Malc <cincenko@outlook.com>
2019-10-13 20:56:40 +0200

    update entry script

--- backend/start_django.sh
+++ backend/start_django.sh
@@ -4,8 +4,7 @@ set -ex
 
 # Clear the existing files before trying to copy or link the original file.
 python3 manage.py collectstatic --no-input
-python3 manage.py makemigrations core
-python3 manage.py makemigrations userMng
+python3 manage.py makemigrations core userMng --no-input
 python3 manage.py migrate
 python3 manage.py graph_models -a -g -o output/class_diagramm.png
 gunicorn melive.wsgi:application --log-level=info -b :8123 --workers 3
