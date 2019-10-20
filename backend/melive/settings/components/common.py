30c7adc13743d710410d72bad67c99bb934c2f04
dmpe <cincenko@outlook.com>
2019-10-20 16:40:43 +0200

    update frontend docker file and common

--- backend/melive/settings/components/common.py
+++ backend/melive/settings/components/common.py
@@ -67,13 +67,12 @@ INSTALLED_APPS = [
     "pinax.messages",
     "graphene_django",
     "debug_toolbar",
-    "graphiql_debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    'graphiql_debug_toolbar.middleware.DebugToolbarMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
