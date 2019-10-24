ddbb7603a6297109779256c88140a3df91533bf1
dmpe <cincenko@outlook.com>
2019-10-24 23:29:09 +0200

    add more hosts

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   productionSourceMap: false,
   runtimeCompiler: true,
   devServer: {
+    host: '0.0.0.0',
     allowedHosts: [
       'melive.xyz',
       'frontend.melive.xyz',
