831f22e4903012daf7cde8664bed12b831c173dd
dmpe <cincenko@outlook.com>
2019-10-27 17:41:37 +0100

    steage after setting up javascript lint

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,41 +1,42 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Contact from './views/Contact.vue';
-import Home from './views/Home.vue';
-import Privacy from './views/Privacy.vue';
-import Terms from './views/Terms.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Contact from "./views/Contact.vue";
+import Home from "./views/Home.vue";
+import Privacy from "./views/Privacy.vue";
+import Terms from "./views/Terms.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: "/",
+      name: "home",
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
-      path: '/contact',
-      name: 'contact',
-      component: Contact,
+      path: "/contact",
+      name: "contact",
+      component: Contact
     },
     {
-      path: '/terms',
-      name: 'terms',
-      component: Terms,
+      path: "/terms",
+      name: "terms",
+      component: Terms
     },
     {
-      path: '/privacy',
-      name: 'privacy',
-      component: Privacy,
-    },
-  ],
+      path: "/privacy",
+      name: "privacy",
+      component: Privacy
+    }
+  ]
 });
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,7 @@ module.exports = {
   productionSourceMap: false,
   runtimeCompiler: true,
   devServer: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'melive.xyz',
-      'frontend.melive.xyz',
-      'backend',
-      'localhost'
-    ]
+    host: "0.0.0.0",
+    allowedHosts: ["melive.xyz", "frontend.melive.xyz", "backend", "localhost"]
   }
-}
+};
