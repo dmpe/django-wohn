dec801919f728e66f46d60127a2bbd946ddcec4b
dmpe <cincenko@outlook.com>
2019-11-17 00:20:57 +0100

    customize a bit markdown display

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -16,6 +16,15 @@ import { createHTMLfromMarkdown } from "@/support.ts";
 
 createHTMLfromMarkdown("https://raw.githubusercontent.com/dmpe/django-wohn/master/README.md").then(response => {
   const converter = new showdown.Converter();
+  converter.setOption('ghCompatibleHeaderId', true);
+  converter.setOption('tables', true);
+  converter.setOption('ghCodeBlocks', true);
+  converter.setOption('tasklists', true);
+  converter.setOption('smartIndentationFix', true);
+  converter.setOption('openLinksInNewWindow', true);
+  converter.setOption('emoji', true);
+  showdown.setFlavor('github');
+
   const html = converter.makeHtml(response.data);
 
   const divRender: HTMLDivElement = document.getElementById("renderMarkdownContent") as HTMLDivElement;
