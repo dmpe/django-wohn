b4f3e9b2bb3085d904e8e8acc5430909ba7f7b02
dmpe <cincenko@outlook.com>
2019-12-08 14:25:36 +0100

    update package and rename queyr

--- frontend/src/graphql/returnLastFive.ts
+++ frontend/src/graphql/returnLastFive.ts
@@ -1,7 +1,10 @@
 import gql from "graphql-tag";
 
 export const returnLastFive = gql`query {
-  homeProperties{
+  // rename query here because vue either needs to update name
+  // be renamed here
+  // see https://github.com/vuejs/vue-apollo/issues/700#issuecomment-511418849
+  returnLastFive: homeProperties {
     id,
     propertyCreated,
     propertyOfferedBy {
