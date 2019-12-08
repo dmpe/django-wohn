a33e3cc31863bd85c8d33e8f52bff3505f9a0be6
dmpe <cincenko@outlook.com>
2019-12-08 14:30:53 +0100

    bad comments

--- frontend/src/graphql/returnLastFive.ts
+++ frontend/src/graphql/returnLastFive.ts
@@ -1,9 +1,9 @@
 import gql from "graphql-tag";
 
+// rename query here because vue either needs to update name
+// be renamed here
+// see https://github.com/vuejs/vue-apollo/issues/700#issuecomment-511418849 */
 export const returnLastFive = gql`query {
-  // rename query here because vue either needs to update name
-  // be renamed here
-  // see https://github.com/vuejs/vue-apollo/issues/700#issuecomment-511418849
   returnLastFive: homeProperties {
     id,
     propertyCreated,
